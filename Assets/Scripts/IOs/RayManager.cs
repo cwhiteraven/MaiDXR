ccc6469094f4c9d2e3816fb739dffb988b2621c9
laproliliu <77503447+laproliliu@users.noreply.github.com>
2025-03-21 14:04:20 -0700

    Add Rigidbody

--- Assets/Scripts/IOs/RayManager.cs
+++ Assets/Scripts/IOs/RayManager.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
+
 
 public class RayManager : MonoBehaviour
 {
     public bool RaySwitch = true;
     public float Distance = 0.4f;
-    XRRayInteractor interactor;
-    XRInteractorLineVisual lineVisual;
+    UnityEngine.XR.Interaction.Toolkit.Interactors.XRRayInteractor interactor;
+    UnityEngine.XR.Interaction.Toolkit.Interactors.Visuals.XRInteractorLineVisual lineVisual;
     LineRenderer lineRenderer;
     void Start()
     {
-        interactor = GetComponent<XRRayInteractor>();
-        lineVisual = GetComponent<XRInteractorLineVisual>();
+        interactor = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactors.XRRayInteractor>();
+        lineVisual = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactors.Visuals.XRInteractorLineVisual>();
         lineRenderer = lineVisual.GetComponent<LineRenderer>();
     }
 
