98818fa3d1e2581a0b86ce104728537131a9193a
laproliliu <77503447+laproliliu@users.noreply.github.com>
2025-06-19 22:42:50 -0700

    yeag

--- Assets/Scripts/IOs/Serial/ButtonManager.cs
+++ Assets/Scripts/IOs/Serial/ButtonManager.cs
@@ -1,43 +1,21 @@
 using System;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
-    int ButtonType;
-    private int _insideColliderCount = 0;
+    [DllImport("mai2io.dll")]
 
-    public static event Action buttonDidChange;
-    void Start()
-    {
-        ButtonType = (int)Enum.Parse(typeof(Button), gameObject.name);
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        _insideColliderCount += 1;
-        SerialManager.ChangeTouch(true, (int)ButtonType, true);
-        buttonDidChange?.Invoke();
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        _insideColliderCount -= 1;
-        _insideColliderCount = Mathf.Max(0, _insideColliderCount);
-        if (_insideColliderCount == 0)
-        {
-            SerialManager.ChangeTouch(true, (int)ButtonType, false);
-            buttonDidChange?.Invoke();
-        }
-    }
-    enum Button
-    {
-        MAI2_IO_GAMEBTN_1 = 0x01,
-        MAI2_IO_GAMEBTN_2 = 0x02,
-        MAI2_IO_GAMEBTN_3 = 0x04,
-        MAI2_IO_GAMEBTN_4 = 0x08,
-        MAI2_IO_GAMEBTN_5 = 0x10,
-        MAI2_IO_GAMEBTN_6 = 0x20,
-        MAI2_IO_GAMEBTN_7 = 0x40,
-        MAI2_IO_GAMEBTN_8 = 0x80,
-        MAI2_IO_GAMEBTN_SELECT = 0x100,
-    };
+    private int button;
 
+    private static extern void io_send_button(uint index, bool value);
+    void OnCollisionEnter(Collision collision)
+    {
+        io_send_button((uint)button, true);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        io_send_button((uint)button, false);
+    }
 }
